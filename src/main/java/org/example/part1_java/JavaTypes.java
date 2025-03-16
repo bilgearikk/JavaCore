52f0f8d5bae43d86cc1046d0b0a1b63cc9b6a84d
bilgearikk <127047435+bilgearikk@users.noreply.github.com>
2025-03-16 03:12:07 +0200

    part1

--- src/main/java/org/example/part1_java/JavaTypes.java
+++ src/main/java/org/example/part1_java/JavaTypes.java
@@ -1,5 +1,9 @@
 package org.example.part1_java;
 
+// single comment
 public class JavaTypes {
-
+    public static void main(String[] args) {
+        System.out.println("Tam zamanlı mühendis");
+    }
 }
+
