16771daed97a8726ab0af34376c28ee863b77bf1
bilgearikk <127047435+bilgearikk@users.noreply.github.com>
2025-04-14 22:18:26 +0300

    commit

---
+++ src/main/java/org/example/_04_Operators.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class _04_Operators {
+    public static void main(String[] args) {
+        // operators + - * /
+        int number1 = 23 , number2 = 7;
+        int addition = number1 + number2;
+        System.out.println(addition);
+
+        int substraction = number1 - number2;
+        System.out.println(substraction);
+
+        int multiplication = number1 * number2;
+        System.out.println(multiplication);
+
+        int division = number1 / number2;
+        System.out.println(division);
+
+        int moduls = number1%number2;
+        System.out.println(moduls);
+
+    }
+}
